15336ca27b437cdee7ff0ed663220f2917b368f5
VishwanathSR <161269352+VishwanathSR@users.noreply.github.com>
2024-09-27 10:57:29 +0530

    Delete templates directory

--- templates/static/js/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('form').addEventListener('submit', function (e) {
-    const name = document.getElementById('name').value;
-    const message = name 
-        ? `Hello, ${name}, you can vote!` 
-        : `Hello, ${name}, you cannot vote.`;
-    alert(message);
-});
